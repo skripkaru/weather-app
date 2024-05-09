59ca4c3a241d7615d21b6bef1be21655ca192871
Evgeniy Skripkaru <skripkaru.evgeniy@gmail.com>
2024-05-09 20:49:18 +0300

    Refactor: added icons and skeleton, updated city validation

--- src/App.vue
+++ src/App.vue
@@ -1,77 +1,128 @@
 <script setup>
 import {computed, ref} from "vue";
 import axios from "axios";
-import { message } from 'ant-design-vue';
+import {message} from 'ant-design-vue';
+import IconHumidity from "@/components/icons/IconHumidity.vue";
+import IconWind from "@/components/icons/IconWind.vue";
+import IconThermometer from "@/components/icons/IconThermometer.vue";
+import IconThermometerExterior from "@/components/icons/IconThermometerExterior.vue";
 
+const isLoading = ref(false)
 const city = ref('')
 const info = ref(null)
 const cityName = computed(() => city.value ? city.value : 'your city')
+const isValidCity = computed(() => /^[a-zA-Z\s]+$/.test(city.value.trim()));
 
 const getWeather = async () => {
-  if(city.value.trim().length < 2){
+  if (!isValidCity.value || city.value.trim().length < 2) {
     message.error('Please enter a valid city name');
     return false
   }
 
   try {
+    isLoading.value = true;
     const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city.value}&units=metric&appid=${import.meta.env.VITE_API_KEY}`);
     info.value = response.data;
   } catch (error) {
     message.error(error.message);
+  } finally {
+    isLoading.value = false;
   }
 }
 </script>
 
 <template>
-  <div class="grid">
+  <div class="wrapper">
+    <h1 class="title">
+      Weather in
+      <span class="description">{{ cityName }}</span>
+    </h1>
     <ACard>
-      <h1>Weather in {{cityName}}</h1>
       <div class="search">
-        <AInput placeholder="Enter city" v-model:value="city" />
-        <AButton type="primary" v-show="city" @click="getWeather">Get weather</AButton>
+        <AInput placeholder="Enter city" v-model:value="city"/>
+        <AButton type="primary" :disabled="!isValidCity" @click="getWeather">Get weather</AButton>
       </div>
     </ACard>
     <AList v-if="info" bordered>
       <AListItem>
         <AListItemMeta>
-          <template #title>Temperature</template>
-          <template #description>{{ info.main.temp }}</template>
+          <template #avatar>
+            <img
+              :src="`https://openweathermap.org/img/w/${info.weather[0].icon}.png`"
+              :alt="info.weather[0].description"
+              width="32px"
+              height="32px"
+            >
+          </template>
+          <template #title>
+            <span class="description">{{ info.weather[0].description }}</span>
+          </template>
+          <template #description>{{ info.main.temp }} °C</template>
         </AListItemMeta>
       </AListItem>
       <AListItem>
         <AListItemMeta>
+          <template #avatar>
+            <IconThermometer/>
+          </template>
           <template #title>Feels like</template>
-          <template #description>{{ info.main.feels_like }}</template>
+          <template #description>{{ info.main.feels_like }} °C</template>
         </AListItemMeta>
       </AListItem>
       <AListItem>
         <AListItemMeta>
-          <template #title>Minimum temperature</template>
-          <template #description>{{ info.main.temp_min }}</template>
+          <template #avatar>
+            <IconThermometerExterior/>
+          </template>
+          <template #title>Min/Max</template>
+          <template #description>{{ info.main.temp_min }}/{{ info.main.temp_max }} °C</template>
         </AListItemMeta>
       </AListItem>
       <AListItem>
         <AListItemMeta>
-          <template #title>Maximum temperature</template>
-          <template #description>{{ info.main.temp_max }}</template>
+          <template #avatar>
+            <IconHumidity/>
+          </template>
+          <template #title>Humidity</template>
+          <template #description>{{ info.main.humidity }} %</template>
+        </AListItemMeta>
+      </AListItem>
+      <AListItem>
+        <AListItemMeta>
+          <template #avatar>
+            <IconWind/>
+          </template>
+          <template #title>Wind</template>
+          <template #description>{{ info.wind.speed }} m/s</template>
         </AListItemMeta>
       </AListItem>
     </AList>
+    <ASkeleton v-else :loading="isLoading" active avatar />
   </div>
-
 </template>
 
 <style scoped>
-.grid {
+.wrapper {
   width: 100%;
-  max-width: 400px;
+  max-width: 500px;
+  padding: 0 16px;
+  margin: 40px auto;
   display: grid;
   gap: 16px;
 }
 
-.search {
-  display: flex;
-  gap: 8px;
+.title {
+  margin: 0;
+  text-align: center;
 }
 
+.description {
+  text-transform: capitalize;
+}
+
+.search {
+  display: grid;
+  grid-template-columns: 1fr auto;
+  gap: 8px;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -3,12 +3,13 @@ import 'ant-design-vue/dist/reset.css';
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import { Button, Card, List, Input } from 'ant-design-vue';
+import { Button, Card, List, Input, Skeleton } from 'ant-design-vue';
 
 const app = createApp(App);
 app.use(Button);
 app.use(Card);
 app.use(List);
 app.use(Input);
+app.use(Skeleton);
 
 app.mount('#app')
