48292d942b74ab68960e7af590c954b036f4903b
Evgeniy Skripkaru <skripkaru.evgeniy@gmail.com>
2024-05-12 22:49:02 +0300

    Refactor: added layout & update validation

--- src/App.vue
+++ src/App.vue
@@ -10,13 +10,12 @@ import IconThermometerExterior from "@/components/icons/IconThermometerExterior.
 const isLoading = ref(false)
 const city = ref('')
 const info = ref(null)
-const cityName = computed(() => city.value ? city.value : 'your city')
-const isValidCity = computed(() => /^[a-zA-Z\s]+$/.test(city.value.trim()));
+const cityName = computed(() => city.value ? city.value : 'Your city')
 
 const getWeather = async () => {
-  if (!isValidCity.value || city.value.trim().length < 2) {
+  if (city.value.trim().length < 2) {
     message.error('Please enter a valid city name');
-    return false
+    return
   }
 
   try {
@@ -32,97 +31,91 @@ const getWeather = async () => {
 </script>
 
 <template>
-  <div class="wrapper">
-    <h1 class="title">
-      Weather in
-      <span class="description">{{ cityName }}</span>
-    </h1>
-    <ACard>
-      <div class="search">
-        <AInput placeholder="Enter city" v-model:value="city"/>
-        <AButton type="primary" :disabled="!isValidCity" @click="getWeather">Get weather</AButton>
+  <ALayout>
+    <ALayoutContent class="content">
+      <div class="wrapper">
+        <ATypographyTitle class="title">Weather in {{ cityName }}</ATypographyTitle>
+        <AForm @submit="getWeather">
+          <AInput placeholder="Enter city" v-model:value="city"/>
+        </AForm>
+        <AList v-if="info" bordered>
+          <AListItem>
+            <AListItemMeta>
+              <template #avatar>
+                <img
+                  :src="`https://openweathermap.org/img/w/${info.weather[0].icon}.png`"
+                  :alt="info.weather[0].description"
+                  width="32px"
+                  height="32px"
+                >
+              </template>
+              <template #title>
+                <span class="description">{{ info.weather[0].description }}</span>
+              </template>
+              <template #description>{{ info.main.temp }} °C</template>
+            </AListItemMeta>
+          </AListItem>
+          <AListItem>
+            <AListItemMeta>
+              <template #avatar>
+                <IconThermometer/>
+              </template>
+              <template #title>Feels like</template>
+              <template #description>{{ info.main.feels_like }} °C</template>
+            </AListItemMeta>
+          </AListItem>
+          <AListItem>
+            <AListItemMeta>
+              <template #avatar>
+                <IconThermometerExterior/>
+              </template>
+              <template #title>Min/Max</template>
+              <template #description>{{ info.main.temp_min }}/{{ info.main.temp_max }} °C</template>
+            </AListItemMeta>
+          </AListItem>
+          <AListItem>
+            <AListItemMeta>
+              <template #avatar>
+                <IconHumidity/>
+              </template>
+              <template #title>Humidity</template>
+              <template #description>{{ info.main.humidity }} %</template>
+            </AListItemMeta>
+          </AListItem>
+          <AListItem>
+            <AListItemMeta>
+              <template #avatar>
+                <IconWind/>
+              </template>
+              <template #title>Wind</template>
+              <template #description>{{ info.wind.speed }} m/s</template>
+            </AListItemMeta>
+          </AListItem>
+        </AList>
+        <ASkeleton v-else :loading="isLoading" active avatar/>
       </div>
-    </ACard>
-    <AList v-if="info" bordered>
-      <AListItem>
-        <AListItemMeta>
-          <template #avatar>
-            <img
-              :src="`https://openweathermap.org/img/w/${info.weather[0].icon}.png`"
-              :alt="info.weather[0].description"
-              width="32px"
-              height="32px"
-            >
-          </template>
-          <template #title>
-            <span class="description">{{ info.weather[0].description }}</span>
-          </template>
-          <template #description>{{ info.main.temp }} °C</template>
-        </AListItemMeta>
-      </AListItem>
-      <AListItem>
-        <AListItemMeta>
-          <template #avatar>
-            <IconThermometer/>
-          </template>
-          <template #title>Feels like</template>
-          <template #description>{{ info.main.feels_like }} °C</template>
-        </AListItemMeta>
-      </AListItem>
-      <AListItem>
-        <AListItemMeta>
-          <template #avatar>
-            <IconThermometerExterior/>
-          </template>
-          <template #title>Min/Max</template>
-          <template #description>{{ info.main.temp_min }}/{{ info.main.temp_max }} °C</template>
-        </AListItemMeta>
-      </AListItem>
-      <AListItem>
-        <AListItemMeta>
-          <template #avatar>
-            <IconHumidity/>
-          </template>
-          <template #title>Humidity</template>
-          <template #description>{{ info.main.humidity }} %</template>
-        </AListItemMeta>
-      </AListItem>
-      <AListItem>
-        <AListItemMeta>
-          <template #avatar>
-            <IconWind/>
-          </template>
-          <template #title>Wind</template>
-          <template #description>{{ info.wind.speed }} m/s</template>
-        </AListItemMeta>
-      </AListItem>
-    </AList>
-    <ASkeleton v-else :loading="isLoading" active avatar />
-  </div>
+    </ALayoutContent>
+  </ALayout>
 </template>
 
 <style scoped>
+.content {
+  min-height: 100vh;
+  padding: 16px;
+}
+
 .wrapper {
   width: 100%;
-  max-width: 500px;
-  padding: 0 16px;
-  margin: 40px auto;
+  max-width: 400px;
+  padding: 16px;
+  margin: 16px auto;
   display: grid;
   gap: 16px;
+  border-radius: 20px;
+  background-color: #fff;
 }
 
 .title {
-  margin: 0;
   text-align: center;
-}
-
-.description {
-  text-transform: capitalize;
-}
-
-.search {
-  display: grid;
-  grid-template-columns: 1fr auto;
-  gap: 8px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,9 +3,12 @@ import 'ant-design-vue/dist/reset.css';
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import { Button, Card, List, Input, Skeleton } from 'ant-design-vue';
+import { Typography, Layout, Form, Button, Card, List, Input, Skeleton } from 'ant-design-vue';
 
 const app = createApp(App);
+app.use(Typography);
+app.use(Layout);
+app.use(Form);
 app.use(Button);
 app.use(Card);
 app.use(List);
